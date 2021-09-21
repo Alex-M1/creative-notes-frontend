caa7f0c9d701ff4a856555ca46c3d77b2481f908
SkrypnikMR <Skripnikmrw@gmail.com>
2021-09-21 23:11:12 +0300

    added type to store in testHelper

--- __tests__/testHelper.tsx
+++ __tests__/testHelper.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { shallow, mount, ShallowWrapper, ReactWrapper } from 'enzyme';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Store } from 'redux';
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const shallowSmart = (component: React.FC, store: any): ShallowWrapper => {
+export const shallowSmart = (component: React.FC, store: Store): ShallowWrapper => {
   const core = store
     ? <Provider store={store}>{component}</Provider>
     : component;
   const test = <Router>{core}</Router>;
   return shallow(test);
 };
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const mountSmart = (component: React.FC, store: any): ReactWrapper => {
+
+export const mountSmart = (component: React.FC, store: Store): ReactWrapper => {
   const core = store
     ? <Provider store={store}>{component}</Provider>
     : component;
